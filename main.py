312e0732e6076a253c5ef5f187ff40647243579f
Rmjp <rmjp15.rmt@gmail.com>
2023-06-29 00:50:18 +0700

    fix

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def hw_send(lab_name, file_name):
     check_connect()
     print('hw-send '+lab_name+' '+file_name)
     stdin, stdout, stderr = client.exec_command('hw-send '+lab_name+' '+file_name)
-    return stdout.read().decode('utf-8') + stderr.read().decode('utf-8')
+    return stdout.read().decode('utf-8') + stderr.read().decode('utf-8') + "asdf"
 
 def send_file_to_server(file_name):
     check_connect()
